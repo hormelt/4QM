0756056d4b9aaf7b39911e73416070bad0665784
JustinTauber <j.tauber.git@gmail.com>
2016-09-21 10:11:03 -0400

    Fake Data maker function
    To automate the fake data generation I have made it into a function.
    Furthermore I added errorbars to the test script.

--- test.m
+++ test.m
@@ -3,24 +3,24 @@ System = 'data/S0_50N0_10SIM';
 % Load 
 load([System '/fov1_track.mat']);
 % Calculate MSD
-knownMSD = calcMSD(Tracks,16.25,0);
+knownMSD = calcMSD(Tracks,1,0);
 load([System '/movie4QMData.mat'])
-QMMSD = CorrectedMSD;
+QMMSD = correctedMSDs;
 preTracks = Tracks;
 preTracks(:,3:4) = [];
-preMSD = calcMSD(preTracks,16.25,0);
+preMSD = calcMSD(preTracks,1,0);
 %Plot MSDs
 
 fig1 = figure();
 whitebg(fig1,[1,1,1])
 scatter(1:size(knownMSD,1),knownMSD(:,1),'k')
 hold on
-scatter(1:size(preMSD,1),preMSD(:,1),'g')
+errorbar(1:size(preMSD,1),preMSD(:,1),preMSD(:,2)/sqrt(size(preMSD,1)),'o','Color','g')
 hold on
-scatter(1:size(MSD,1),MSD(:,1),'b')
+errorbar(1:size(MSDs,1),MSDs(:,1),MSDs(:,2)/sqrt(size(MSDs,1)),'o','Color','b')
 hold on
-scatter(1:size(QMMSD,1),QMMSD(:,1),'r')
-ylim([100,1000])
+errorbar(1:size(QMMSD,1),QMMSD(:,1),QMMSD(:,2)/sqrt(size(QMMSD,1)),'o','Color','r')
+ylim([min(QMMSD(2:end,1))*0.8,max(QMMSD(:,1))*1.5])
 box on
 set(gca,'xscale','log','yscale','log')
 xlabel('\tau [frame]');
